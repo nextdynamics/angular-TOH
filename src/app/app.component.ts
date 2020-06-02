147c54e9d8187dab67748785c31352a76ad35e3b
nextdynamics <43843672+nextdynamics@users.noreply.github.com>
2020-06-02 16:45:57 +0200

    ✅ 1. The Hero Editor (for real this time)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '👋 Tuhr off Hierohs 🤙';
+  title = '👋 Turoff Hieroz 🤙';
 }
