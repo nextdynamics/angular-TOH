926f59f83e71f109b5c7519bbdd61f33db0e22f1
nextdynamics <43843672+nextdynamics@users.noreply.github.com>
2020-06-02 17:10:51 +0200

    ✅ 2. Display a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Luftmann' },
+  { id: 12, name: 'Medizino' },
+  { id: 13, name: 'Superfrau' },
+  { id: 14, name: 'Cholesterol' },
+  { id: 15, name: 'Vagina' },
+  { id: 16, name: 'Explosionsmann' },
+  { id: 17, name: 'Karsten' },
+  { id: 18, name: 'Dr. Doktor' },
+  { id: 19, name: 'Puploch' },
+  { id: 20, name: 'Zerstörmacher' }
+];
