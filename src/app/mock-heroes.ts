5216f8e0c215e70f0d10ae3754b9c486605d4b8a
nextdynamics <43843672+nextdynamics@users.noreply.github.com>
2020-06-03 17:39:22 +0200

    improved heronames 👨🏻‍🔬

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Luftmann' },
+  { id: 11, name: 'Luftmann Senior' },
   { id: 12, name: 'Medizino' },
   { id: 13, name: 'Superfrau' },
   { id: 14, name: 'Cholesterol' },
   { id: 15, name: 'Vagina' },
   { id: 16, name: 'Explosionsmann' },
   { id: 17, name: 'Karsten' },
-  { id: 18, name: 'Dr. Doktor' },
-  { id: 19, name: 'Puploch' },
+  { id: 18, name: 'Dr. Dog-Thor' },
+  { id: 19, name: 'Pobrand II.' },
   { id: 20, name: 'Zerstörmacher' }
 ];
