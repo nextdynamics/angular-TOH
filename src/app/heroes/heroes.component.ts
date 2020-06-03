e2fc132735733351f0cfbdbc3bc97af3e8ecb09d
nextdynamics <43843672+nextdynamics@users.noreply.github.com>
2020-06-03 21:21:17 +0200

    ✅ 5. Add In-app Navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,17 +12,11 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
-  selectedHero: Hero;
   heroes: Hero[];
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   ngOnInit(): void {
